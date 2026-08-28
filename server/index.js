b34561b32be5b919c8f6964cb95b21f4f6742de0
Yauhen Belavus <yauhenbelavus@gmail.com>
2026-08-28 16:11:31 +0200

    ocr api fixed

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
 const sqlite3 = require('sqlite3');
-const path = require('path');
 const multer = require('multer');
 const fs = require('fs');
 
